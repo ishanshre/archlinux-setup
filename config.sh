95a4ee56b23cb018657f6047762ae8d56468269f
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-04-22 18:29:27 +0545

    feat: update hyprland config

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 echo "Configuring location"
 ln -sf /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
 hwclock --systohc
-
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
--- hyprland.sh
+++ hyprland.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-paru -S hyprland-git polkit-kde-agent dunst grimblast rofi rofi-emoji wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep waybar-hyprland-git
+paru -S hyprland-git polkit-kde-agent dunst grimblast rofi rofi-emoji wl-clipboard wf-recorder wlogout grimblast-git hyprpicker-git hyprpaper-git xdg-desktop-portal-hyprland-git ffmpegthumbnailer tumbler wtype colord imagemagick swaylock-effects qt5-wayland qt6-wayland ripgrep waybar-hyprland-git sddm-git
+sudo systemctl enable sddm
